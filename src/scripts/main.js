ba895f1aa9d18b6612a7ea2b775c18c5c10555f3
Iryna Shevkun <sevkunirina65@gmail.com>
2025-05-07 12:11:26 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('img[id="largeImg"]');
+const list = document.querySelector('ul');
+
+list.addEventListener('click', (listEvent) => {
+  listEvent.preventDefault();
+
+  const listItem = listEvent.target.closest('.list-item__link');
+
+  largeImg.src = listItem.href;
+});
