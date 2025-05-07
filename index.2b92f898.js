2116a8484136be94a0f175fed6a83d83a05dbd11
Iryna Shevkun <sevkunirina65@gmail.com>
2025-05-07 12:20:09 +0300

    make build

---
+++ index.2b92f898.js
@@ -0,0 +1,2 @@
+var e=document.querySelector('img[id="largeImg"]');document.querySelector("ul").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");r&&(e.src=r.href)});
+//# sourceMappingURL=index.2b92f898.js.map
